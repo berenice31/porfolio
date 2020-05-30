4fe0e2eb95a709e9d57f4567bb7552d72cf689f5
berenice31 <berenice.lortie@gmail.com>
2020-05-30 23:15:02 +0200

    home page

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import Header from 'src/components/Header';
+import './style.scss';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <Header />
+  </div>
+);
+
+// == Export
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './style.scss';
+import photo from './work.jpg';
+
+const Header = () => (
+  <header className="header">
+    <div>
+      <img className="header-image" src={photo} alt="espace de travail" />
+    </div>
+    <div className="contact">
+      <p className="contact-item"> 
+        <ion-icon name="call-outline" className="contact-item-icon" />    <p className="contact-item-text">06.29.68.13.36</p>
+      </p>
+      <p className="contact-item">
+        <ion-icon name="mail-outline" className="contact-item-icon" /> <p className="contact-item-text mail"><a>berenice.lortie@gmail.com</a></p>
+      </p>
+    </div>
+    <nav className="nav">
+      <a href=""><p className="nav-item"> <ion-icon name="home-outline" /></p></a>
+      <a href=""><p className="nav-item"> Mon parcours</p></a>
+      <a href=""><p className="nav-item"> Compétences</p></a>
+      <a href=""><p className="nav-item"> Mes projets </p></a>
+      <a href=""><p className="nav-item"> Contact </p></a>
+    </nav>
+  </header>
+
+);
+export default Header;
